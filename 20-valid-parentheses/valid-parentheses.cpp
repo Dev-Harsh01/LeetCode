4fb613258ad257b6a834361094d0d9644129eba7
Harsh Gupta <hg1194362@gmail.com>
2025-09-04 18:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (22.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -4,21 +4,25 @@ public:
         stack<char> st;
 
         for(int i=0;i<s.size();i++){
-            if (s[i] == '(' || s[i] == '{' || s[i] == '['){
-                st.push(s[i]);}
-
-             else {
-                if (st.empty()) return false; // no matching opener
-                if ((s[i] == ')' && st.top() != '(') ||
-                    (s[i] == '}' && st.top() != '{') ||
-                    (s[i] == ']' && st.top() != '[')) {
-                    return false; // mismatched pair
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
                 }
-                st.pop();
+
+                if((s[i]==')' && st.top() != '(') || (s[i]=='}' && st.top() != '{') || (s[i]==']' && st.top() != '[')){
+                    return false;
+                }
+
+                st.pop(); 
             }
 
         }
 
         return st.empty();
+
+        
     }
 };
